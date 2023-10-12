6ee69958ca95156e14e8957cdefdfacf107d60f2
Lucas Santana <122113277+llink7@users.noreply.github.com>
2023-10-12 10:48:44 -0300

    config database

--- src/main/java/br/com/lucasbelato/todolist/user/UserModel.java
+++ src/main/java/br/com/lucasbelato/todolist/user/UserModel.java
@@ -1,7 +1,10 @@
 package br.com.lucasbelato.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
-  private String username;
+  private String userName;
   private String name;
   private String password;
 

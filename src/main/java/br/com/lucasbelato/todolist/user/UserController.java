45ee5a375ba583eecbd92e849f98fb5ef0493c2f
Lucas Santana <122113277+llink7@users.noreply.github.com>
2023-10-15 22:10:08 -0300

    feat: 🛠 passwordHashred

--- src/main/java/br/com/lucasbelato/todolist/user/UserController.java
+++ src/main/java/br/com/lucasbelato/todolist/user/UserController.java
@@ -1,9 +1,9 @@
 package br.com.lucasbelato.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +23,9 @@ public class UserController {
 
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
     }
+
+    var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+    userModel.setPassword(passwordHashred);
     var userCreated = this.userRepository.save(userModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }

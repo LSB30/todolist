d5108ae705ba631109fe6a4ae1a8cf461c183b60
Lucas Santana <122113277+llink7@users.noreply.github.com>
2023-10-16 22:49:19 -0300

    Refactor: Authorization

--- src/main/java/br/com/lucasbelato/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/lucasbelato/todolist/filter/FilterTaskAuth.java
@@ -3,8 +3,12 @@ package br.com.lucasbelato.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.lucasbelato.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,6 +16,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private IUserRepository userRepository;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -24,13 +31,26 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     byte[] authDecode = Base64.getDecoder().decode(authEncoded);
     var authString = new String(authDecode);
     System.out.println("Authorization");
-    System.out.println(authEncoded);
-    System.out.println(authDecode);
-    System.out.println(authString);
+
     String[] credentials = authString.split(":");
     String username = credentials[0];
     String password = credentials[1];
-    filterChain.doFilter(request, response);
+
+    var user = this.userRepository.findByUsername(username);
+    if (user == null) {
+      response.sendError(401);
+    } else {
+
+      var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+      if (passwordVerify.verified) {
+        filterChain.doFilter(request, response);
+
+      } else {
+        response.sendError(401);
+
+      }
+
+    }
   }
 
 }
